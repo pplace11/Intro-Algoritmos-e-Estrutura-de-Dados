831103bd491e8505fbdcae261a9b19410822ea8e
Pedro Place <pedro.place@hotmail.com>
2024-10-16 18:31:22 +0100

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,20 @@
+#  Exercício 1 – Variáveis e aritmética em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+a = int(input("Qual é o primeiro valor? "))
+b = int(input("Qual é o segundo valor? "))
+soma = a+b
+mul = a*b
+sub = a-b
+div = a/b
+divinteira = a//b
+restodiv = a%b
+exponecial = a**b
+print(f"Os calculos sao: soma {soma}, multiplicação {mul}, subtração {sub}, divisão {div}, divisão de inteiros {divinteira}, resto da divisão {restodiv}, exponecial {exponecial}")
+
