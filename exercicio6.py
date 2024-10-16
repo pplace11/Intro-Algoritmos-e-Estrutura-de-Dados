7c21ead5a54489409168ad52045eb6cd4956cb32
Pedro Place <pedro.place@hotmail.com>
2024-10-16 18:31:34 +0100

    Create exercicio6.py

---
+++ exercicio6.py
@@ -0,0 +1,14 @@
+# Exercício 6 – Repetições em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+start_line = 10
+stopwatch = start_line
+while stopwatch >= 0:
+    print(stopwatch)
+    stopwatch = stopwatch -1
