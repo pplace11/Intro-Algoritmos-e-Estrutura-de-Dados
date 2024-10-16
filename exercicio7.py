eb940dfdb0b25e0b41c8222082e763b81a3076da
Pedro Place <pedro.place@hotmail.com>
2024-10-16 18:31:37 +0100

    Create exercicio7.py

---
+++ exercicio7.py
@@ -0,0 +1,13 @@
+#  Exercício 7 – Repetições em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+departure = 10
+timer = departure
+for timer in range(departure, -1, -1):
+    print(timer)
