a87d121b89c372b063f486dcc7554dafa4f7ecf4
Pedro Place <pedro.place@hotmail.com>
2024-11-13 16:59:53 +0000

    Create exercicio2.py

---
+++ PrimeiroTrabalhosDeAula/exercicio2.py
@@ -0,0 +1,31 @@
+# Exercício 2 – Tipos de dados em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+a = 33
+b = 33.3
+c = 10+20
+d = 'Pedro'
+e = True
+f = [1,'a',3]
+g = (2, 'b', 3.6)
+h = {2: 'j', 6: 'f'}
+i = frozenset({5,7})
+j = range(10)
+k = bytes([5,3,4])
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
