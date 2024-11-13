6d9571ba7497c54c78eaef353b4a1650079bc7f0
Pedro Place <pedro.place@hotmail.com>
2024-11-13 17:00:41 +0000

    Delete ProfileInsertion.py

--- ProfileInsertion.py
+++
@@ -1,9 +0,0 @@
-import InsertionSort
-import profile
-
-ns = [10000, 20000, 30000, 40000, 50000]
-
-def selection_sort_test(arr):
-    InsertionSort.insertion_sort(arr, len(arr))
-
-profile.profile_algorithm(selection_sort_test, ns, "Insertion Sort")
