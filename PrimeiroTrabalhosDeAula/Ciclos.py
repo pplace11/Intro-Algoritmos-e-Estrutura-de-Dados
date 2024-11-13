9f96670d836656d230dc57931f0ccf3ed3b333de
Pedro Place <pedro.place@hotmail.com>
2024-11-13 16:59:41 +0000

    Create Ciclos.py

---
+++ PrimeiroTrabalhosDeAula/Ciclos.py
@@ -0,0 +1,21 @@
+# Ciclos em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+ponto_de_partida = 10
+month = ['janeiro', 'fevereiro', 'marco']
+for contador in range(ponto_de_partida, -1, -1):
+    print(contador)
+
+for month in month:
+    print(month)
+
+contador = ponto_de_partida
+while contador >=0:
+    print(contador)
+    contador = contador -1
