8e1b27c7f461edf4608508cb5d7fb2b67f84aa77
Pedro Place <pedro.place@hotmail.com>
2024-11-13 17:00:27 +0000

    Create primeiro.py

---
+++ PrimeiroTrabalhosDeAula/primeiro.py
@@ -0,0 +1,12 @@
+#
+# O meu primeiro programa no PyCharm
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/09/2024
+#
+print("Hello world")
