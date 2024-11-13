e53ab4c40cf3d84b30fe6182452b7599ae792261
Pedro Place <pedro.place@hotmail.com>
2024-11-13 17:00:17 +0000

    Create funcoes.py

---
+++ PrimeiroTrabalhosDeAula/funcoes.py
@@ -0,0 +1,21 @@
+# Funcoes em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+def mostrar(frase):
+    print(frase)
+
+def somar(a,b):
+    return a + b
+a = 33
+b = 44
+print(somar(a,b))
+mostrar("Primeira")
+print("Já voltei da funcao mostrar()")
+mostrar("Segunda")
+mostrar("Terceira")
