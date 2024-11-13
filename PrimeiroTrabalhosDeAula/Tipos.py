e759da595cab061ac500f155d0993d86007eaea1
Pedro Place <pedro.place@hotmail.com>
2024-11-13 17:00:29 +0000

    Create Tipos.py

---
+++ PrimeiroTrabalhosDeAula/Tipos.py
@@ -0,0 +1,21 @@
+# Tipos em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+
+a = 33
+b = 3.5
+c = 'texto'
+d = True
+e = "outra"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
