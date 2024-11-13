75610d69a83530c62f3538519da00fe8ec8065f7
Pedro Place <pedro.place@hotmail.com>
2024-11-13 16:59:47 +0000

    Create Condicoes.py

---
+++ PrimeiroTrabalhosDeAula/Condicoes.py
@@ -0,0 +1,18 @@
+# Intucoes condicionais em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+temperatura = 18
+
+print(f"A temperatura vale: {temperatura} graus")
+if not temperatura > 18 and True:
+    print("Esta frio la fora.")
+    print("Se calhar é inverno")
+else:
+    print("Esta uma temperatura agradavel la fora")
+print(temperatura <=18)
