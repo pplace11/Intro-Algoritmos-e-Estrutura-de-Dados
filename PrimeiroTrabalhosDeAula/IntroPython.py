4bcbadc5a3b09932fef6049b90f29e515c9a2092
Pedro Place <pedro.place@hotmail.com>
2024-11-13 17:00:20 +0000

    Create IntroPython.py

---
+++ PrimeiroTrabalhosDeAula/IntroPython.py
@@ -0,0 +1,22 @@
+# Sthing em Python
+#
+#Curso: Desenvolvimento para Web e Dispositivo Movel
+#
+#UC:Algoritmos e Estrutura de Dados
+#
+#Número:22404914
+#Nome: Pedro Place
+#Data: 10/16/2024
+
+frase = 'A minha primeira string'
+frase1 = 'A minha segunda string'
+print(f'A frase é: "{frase}" e "{frase1}"')
+
+a = 'banana'
+b = 'peras'
+c = 'uvas'
+print('+'.join([a,b,c]))
+print(f"A dimensao de frase1 é: {len(frase1)}")
+print(frase[6:16:2])
+print(frase.find('s'))
+print(frase.replace('a','b'))
